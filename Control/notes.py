16e6b876e2701bdae3dded91b807b386e46aebf2
Grisha-NoSense <Devimp15@mail.ru>
2023-12-19 00:05:26 +0300

    Create notes.py

---
+++ Control/notes.py
@@ -0,0 +1,85 @@
+# Приложение должно запускаться без ошибок, должно уметь сохранять данные
+# в файл, уметь читать данные из файла, делать выборку по дате, выводить на
+# экран выбранную запись, выводить на экран весь список записок, добавлять
+# записку, редактировать ее и удалять.
+from datetime import datetime
+
+
+def note_list():
+    cnt = len(notes.keys())
+    print(f'Кол-во заметок: {cnt}')
+    if cnt > 0:
+        for i, j in notes.items():
+            print(f'{i}:{j[0]}')
+
+
+def note_show():
+    pass
+
+
+def note_update(index):
+    pass
+
+
+def note_open():
+    pass
+
+
+def note_delete(index):
+    pass
+
+
+def note_create(name):
+    notes[max(notes.keys()) + 1] = [name, 'Empty', datetime.strftime("%d/%m/%Y %H:%M:%S"),
+                                    datetime.strftime("%d/%m/%Y %H:%M:%S")]
+    print(f'Заметка {name} была успешно создана! Её идентификатор {max(notes.keys())}')
+
+
+def note_save():
+    pass
+
+
+def check_input(text):
+    if ' ' in text:
+        res = text.split(' ')[1]
+        if res == '':
+            return None
+        else:
+            return res
+    else:
+        return None
+
+
+def choices(choice=None):
+    modif = check_input(choice)
+    if choice == '/help':
+        print("""
+        /list - Показать список всех заметок.
+        /create NAME - Создать новую заметку, где NAME, её название.
+        /delete N - Удалить заметку, где N - её уникальный номер.
+        /update N - Редактировать заметку, где N - её уникальный номер.
+        """)
+    elif choice == '/list':
+        note_list()
+    elif '/create' in choice:
+        if modif:
+            note_create(modif)
+        else:
+            print('Вы не ввели имя заметки!')
+    elif '/delete' in choice:
+        if modif:
+            note_delete(int(modif))
+        else:
+            print('Вы не ввели номер заметки!')
+    elif '/update' in choice:
+        if modif:
+            note_update(int(modif))
+        else:
+            print('Вы не ввели номер заметки!')
+
+
+notes = {}
+
+while True:
+    print('Введите /help для вывода списка команд.')
+    choices(input("Введите команду:").strip())
